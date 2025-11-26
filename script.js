bb8efbfb665e9f0f9975e4e0a92bea6851a02e66
bipo01 <135768513+bipo01@users.noreply.github.com>
2025-11-26 15:19:20 -0400

    meu script para display de tabela

--- script.js
+++ script.js
@@ -11,194 +11,120 @@ let dataGS;
 let headerGS;
 
 nfsPasta.addEventListener("change", () => {
-  nfsPastaArr = [...nfsPasta.files]
-    .map((file) => file.name)
-    .map((name) => {
-      const formattedNameArr = name.split("-");
-      const numeroOS = formattedNameArr[0].trim().split(" ").at(-1);
-      const tipo = formattedNameArr[1].toUpperCase().includes("P") ? "P" : "M";
-      const valor = Number(
-        formattedNameArr[2]
-          .trim()
-          .split(".")[0]
-          .replaceAll(".", "")
-          .replaceAll(",", ".")
-      );
-      return { numeroOS, tipo, valor, origem: "Pasta" };
-    });
+	nfsPastaArr = [...nfsPasta.files]
+		.map((file) => file.name)
+		.map((name) => {
+			const formattedNameArr = name.split("-");
+			const numeroOS = formattedNameArr[0].trim().split(" ").at(-1);
+			const tipo = formattedNameArr[1].toUpperCase().includes("P") ? "P" : "M";
+			const valor = Number(formattedNameArr[2].trim().split(".")[0].replaceAll(".", "").replaceAll(",", "."));
+			return { numeroOS, tipo, valor, origem: "Pasta" };
+		});
 });
 
 document.addEventListener("click", async (e) => {
-  const element = e.target;
+	const element = e.target;
 
-  if (element.id === "coletarDadosGS") {
-    resultado.querySelector(`.numeroOS`).innerHTML = "";
-    resultado.querySelector(`.tipoGS`).innerHTML = "";
-    resultado.querySelector(`.tipoPasta`).innerHTML = "";
-    resultado.querySelector(`.tipoFatura`).innerHTML = "";
-    resultado.querySelector(`.valorGS`).innerHTML = "";
-    resultado.querySelector(`.valorPasta`).innerHTML = "";
-    resultado.querySelector(`.valorFatura`).innerHTML = "";
-    nfsGS = [];
-    const response = await fetch(
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfM0nGoCsDmmp0W15By-T3GP9ncKCtTDSQ2XHgnQHnLQVpjZqQAXe42NpsMIRd9-UkxH34g8y2zo3Q/pub?gid=0&single=true&output=csv"
-    );
+	if (element.id === "coletarDadosGS") {
+		resultado.querySelector(`.numeroOS`).innerHTML = "";
+		resultado.querySelector(`.tipoGS`).innerHTML = "";
+		resultado.querySelector(`.tipoPasta`).innerHTML = "";
+		resultado.querySelector(`.tipoFatura`).innerHTML = "";
+		resultado.querySelector(`.valorGS`).innerHTML = "";
+		resultado.querySelector(`.valorPasta`).innerHTML = "";
+		resultado.querySelector(`.valorFatura`).innerHTML = "";
+		nfsGS = [];
+		const response = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vTfM0nGoCsDmmp0W15By-T3GP9ncKCtTDSQ2XHgnQHnLQVpjZqQAXe42NpsMIRd9-UkxH34g8y2zo3Q/pub?gid=0&single=true&output=csv");
 
-    const csvText = await response.text();
+		const csvText = await response.text();
 
-    const parsed = Papa.parse(csvText, {
-      header: false,
-      skipEmptyLines: true,
-    });
+		const parsed = Papa.parse(csvText, {
+			header: false,
+			skipEmptyLines: true,
+		});
 
-    dataGS = parsed.data.slice(1);
+		dataGS = parsed.data.slice(1);
 
-    headerGS = {};
+		headerGS = {};
 
-    parsed.data[0].forEach((col, i) => (headerGS[`${col}`] = i));
+		parsed.data[0].forEach((col, i) => (headerGS[`${col}`] = i));
 
-    const filteredData = dataGS.filter(
-      (row) =>
-        row[headerGS["Mês de Finalização"]] == mesFinalizacaoGS.value &&
-        row[headerGS["Ano de Finalização"]] == anoFinalizacaoGS.value
-    );
-    let totalPecas = 0;
-    let totalMaoDeObra = 0;
-    let total = 0;
+		const filteredData = dataGS.filter((row) => row[headerGS["Mês de Finalização"]] == mesFinalizacaoGS.value && row[headerGS["Ano de Finalização"]] == anoFinalizacaoGS.value);
+		let totalPecas = 0;
+		let totalMaoDeObra = 0;
+		let total = 0;
 
-    filteredData.forEach((row) => {
-      const valorPecas = toNumberBR(row[headerGS["Peças"]]);
-      const valorMaoDeObra = toNumberBR(row[headerGS["Mão de Obra"]]);
-      const valorTotal = toNumberBR(row[headerGS["Total"]]);
+		filteredData.forEach((row) => {
+			const valorPecas = toNumberBR(row[headerGS["Peças"]]);
+			const valorMaoDeObra = toNumberBR(row[headerGS["Mão de Obra"]]);
+			const valorTotal = toNumberBR(row[headerGS["Total"]]);
 
-      totalPecas += valorPecas;
-      totalMaoDeObra += valorMaoDeObra;
-      total += valorTotal;
-    });
+			totalPecas += valorPecas;
+			totalMaoDeObra += valorMaoDeObra;
+			total += valorTotal;
+		});
 
-    filteredData.forEach((row) => {
-      const numeroOS = row[headerGS["O.S"]];
-      let tipo;
-      let valor;
-      if (toNumberBR(row[headerGS["Peças"]]) > 0) {
-        tipo = "P";
-        valor = toNumberBR(row[headerGS["Peças"]]);
-        nfsGS.push({ numeroOS, tipo, valor, origem: "GS" });
-      }
+		filteredData.forEach((row) => {
+			const numeroOS = row[headerGS["O.S"]];
+			let tipo;
+			let valor;
+			if (toNumberBR(row[headerGS["Peças"]]) > 0) {
+				tipo = "P";
+				valor = toNumberBR(row[headerGS["Peças"]]);
+				nfsGS.push({ numeroOS, tipo, valor, origem: "GS" });
+			}
 
-      if (toNumberBR(row[headerGS["Mão de Obra"]]) > 0) {
-        tipo = "M";
-        valor = toNumberBR(row[headerGS["Mão de Obra"]]);
-        nfsGS.push({ numeroOS, tipo, valor, origem: "GS" });
-      }
-    });
+			if (toNumberBR(row[headerGS["Mão de Obra"]]) > 0) {
+				tipo = "M";
+				valor = toNumberBR(row[headerGS["Mão de Obra"]]);
+				nfsGS.push({ numeroOS, tipo, valor, origem: "GS" });
+			}
+		});
 
-    const todasOSs = [
-      ...new Set([
-        ...(nfsGS?.map((el) => el.numeroOS) ?? []),
-        ...(nfsPastaArr?.map((el) => el.numeroOS) ?? []),
-      ]),
-    ].sort((a, b) => {
-      return Number(a) - Number(b);
-    });
+		const todasOSs = [...new Set([...(nfsGS?.map((el) => el.numeroOS) ?? []), ...(nfsPastaArr?.map((el) => el.numeroOS) ?? [])])].sort((a, b) => {
+			return Number(a) - Number(b);
+		});
 
-    todasOSs.forEach((os) => {
-      const todasNFs = [
-        ...(nfsGS?.filter((nf) => nf.numeroOS == os) ?? []),
-        ...(nfsPastaArr?.filter((nf) => nf.numeroOS == os) ?? []),
-      ].sort((a, b) => {
-        const order = { P: 1, M: 2 };
-        return order[a.tipo] - order[b.tipo];
-      });
+		const origens = ["GS", "Pasta", "Fatura"];
 
-      console.log(todasNFs);
+		todasOSs.forEach((os) => {
+			const todasNFs = [...(nfsGS?.filter((nf) => nf.numeroOS == os) ?? []), ...(nfsPastaArr?.filter((nf) => nf.numeroOS == os) ?? [])].sort((a, b) => {
+				const order = { P: 1, M: 2 };
+				return order[a.tipo] - order[b.tipo];
+			});
 
-      const tipos = ["M", "P"];
+			const todasMO = todasNFs.filter((nf) => nf.tipo === "M");
+			const todasPC = todasNFs.filter((nf) => nf.tipo === "P");
 
-      tipos.forEach((tipo) => {
-        const nfsGS = todasNFs.filter(
-          (n) => n.tipo === tipo && n.origem === "GS"
-        );
-        const nfsPasta = todasNFs.filter(
-          (n) => n.tipo === tipo && n.origem === "Pasta"
-        );
-        const nfsFatura = todasNFs.filter(
-          (n) => n.tipo === tipo && n.origem === "Fatura"
-        );
+			if (todasMO.length > 0) {
+				resultado.querySelector(".numeroOS").insertAdjacentHTML("beforeend", `<h3>${os}</h3>`);
 
-        // Se nenhuma origem tem esse tipo, pula
-        if (
-          nfsGS.length === 0 &&
-          nfsPasta.length === 0 &&
-          nfsFatura.length === 0
-        )
-          return;
+				const obj = origens.map((orig) => {
+					const nf = todasMO.find((el) => el.origem === orig);
+					if (!nf) return { numeroOS: os, tipo: "-", valor: "-", origem: orig };
+					return nf;
+				});
 
-        // Linha
-        resultado
-          .querySelector(".numeroOS")
-          .insertAdjacentHTML("beforeend", `<h3>${os}</h3>`);
+				obj.forEach((nf) => {
+					resultado.querySelector(`.tipo${nf.origem}`).insertAdjacentHTML("beforeend", `<h3>${nf.tipo}</h3>`);
+					resultado.querySelector(`.valor${nf.origem}`).insertAdjacentHTML("beforeend", `<h3>${nf.valor}</h3>`);
+				});
+			}
 
-        // GS
-        if (nfsGS.length > 0) {
-          resultado
-            .querySelector(".tipoGS")
-            .insertAdjacentHTML(
-              "beforeend",
-              `<h3>${tipo == "P" ? "Peças" : "Mão de Obra"}</h3>`
-            );
-          resultado
-            .querySelector(".valorGS")
-            .insertAdjacentHTML("beforeend", `<h3>R$ ${nfsGS[0].valor}</h3>`);
-        } else {
-          resultado
-            .querySelector(".tipoGS")
-            .insertAdjacentHTML("beforeend", `<h3>-</h3>`);
-          resultado
-            .querySelector(".valorGS")
-            .insertAdjacentHTML("beforeend", `<h3>-</h3>`);
-        }
+			if (todasPC.length > 0) {
+				resultado.querySelector(".numeroOS").insertAdjacentHTML("beforeend", `<h3>${os}</h3>`);
 
-        // Pasta
-        if (nfsPasta.length > 0) {
-          resultado
-            .querySelector(".tipoPasta")
-            .insertAdjacentHTML("beforeend", `<h3>${tipo}</h3>`);
-          resultado
-            .querySelector(".valorPasta")
-            .insertAdjacentHTML(
-              "beforeend",
-              `<h3>R$ ${nfsPasta[0].valor}</h3>`
-            );
-        } else {
-          resultado
-            .querySelector(".tipoPasta")
-            .insertAdjacentHTML("beforeend", `<h3>-</h3>`);
-          resultado
-            .querySelector(".valorPasta")
-            .insertAdjacentHTML("beforeend", `<h3>-</h3>`);
-        }
+				const obj = origens.map((orig) => {
+					const nf = todasPC.find((el) => el.origem === orig);
+					if (!nf) return { numeroOS: os, tipo: "-", valor: "-", origem: orig };
+					return nf;
+				});
 
-        // Fatura
-        if (nfsFatura.length > 0) {
-          resultado
-            .querySelector(".tipoFatura")
-            .insertAdjacentHTML("beforeend", `<h3>${tipo}</h3>`);
-          resultado
-            .querySelector(".valorFatura")
-            .insertAdjacentHTML(
-              "beforeend",
-              `<h3>R$ ${nfsFatura[0].valor}</h3>`
-            );
-        } else {
-          resultado
-            .querySelector(".tipoFatura")
-            .insertAdjacentHTML("beforeend", `<h3>-</h3>`);
-          resultado
-            .querySelector(".valorFatura")
-            .insertAdjacentHTML("beforeend", `<h3>-</h3>`);
-        }
-      });
-    });
-  }
+				obj.forEach((nf) => {
+					resultado.querySelector(`.tipo${nf.origem}`).insertAdjacentHTML("beforeend", `<h3>${nf.tipo}</h3>`);
+					resultado.querySelector(`.valor${nf.origem}`).insertAdjacentHTML("beforeend", `<h3>${nf.valor}</h3>`);
+				});
+			}
+		});
+	}
 });
